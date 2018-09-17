725157d131cc8217147b2654f5ff958840e7ab46
Andrew Hoglund <drew@music-machine.local>
2018-09-16 22:51:22 -0400

    add ig to photos page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,14 @@
 class PhotosController < ApplicationController
+
+  before_action :load_instagram
   def index
     @page_title = "Photos"
     @albums = Album.eager_load(:photos).all
   end
 
-  def instagram
+  private
+
+  def load_instagram
     client = Instagram.client(access_token: ENV['INSTAGRAM_ACCESS_TOKEN'])
     @media = client.user_recent_media
   end
