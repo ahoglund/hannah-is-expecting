1643db812ac0768c1c8bc2a4545894a8ced7482f
Andrew Hoglund <>
2019-01-21 23:30:47 -0500

    fix rand image

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,6 @@
 module PagesHelper
+  def random_picture_from(album:)
+    photos = Album.find_by(title: "Gisel & Drew").photos
+    photos.offset(rand(photos.count)).first.image.variant(resize: "300")
+  end
 end
